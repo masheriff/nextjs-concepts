7f5ec11b245e78499b4698fefa1e410fcd950bcc
Mohammed Aamir Sheriff <aamirsheriff@gmail.com>
2025-09-07 00:43:09 +0530

    creating auth instance

---
+++ lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db"; // your drizzle instance
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+  }),
+  
+  //Adding email and password support
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  //Adding support for socialProviders
+  socialProviders: {
+    // Adding support for github
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+});
